3823e005820e6d23d7da36d8c488618198f03dff
Rashif <BSI90945@bsi.co.id>
2025-10-30 14:53:17 +0700

    fix navbar for mobile view

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,14 +5,21 @@ export function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-light bg-white sticky-top">
       <div className="container">
         <Link className="navbar-brand fw-bold" to="/">MyPortfolio</Link>
+        
+        {/* Toggle Button */}
         <button 
           className="navbar-toggler" 
           type="button" 
           data-bs-toggle="collapse" 
           data-bs-target="#navbarNav"
+          aria-controls="navbarNav" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+        
+        {/* Navbar Links */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
